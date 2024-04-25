239f7ce380b57b064b9a98863ea3c718e5df9a57
Yanitsa Nankova <201221011@fdiba.tu-sofia.bg>
2024-04-25 22:52:12 +0200

    Edit pages

--- src/main/frontend/src/components/pages/Home/Home.js
+++ src/main/frontend/src/components/pages/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Home.css';
-//import logo from './logo1';
+import logo from './tictactoe-logo.png';
 
 function Home(){
     
     return (
         <div className='container'>
             <h1 className="title1">Welcome to Tic-Tac-Toe!</h1>
-          
+            <img src= {logo}  alt="Tic-Tac-Toe"/>
             <div className="twoButtons">
                 <div className="firstButton">
                     <a href="/About">How to play Tic-Tac-Toe?</a>
