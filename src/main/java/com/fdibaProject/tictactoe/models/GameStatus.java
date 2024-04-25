41fc313fbff36fa90cc9237bd2d3471672790f18
Elitsa Chakarova <elitsa.chakarova@fdiba.tu-sofia.bg>
2024-04-25 21:35:25 +0200

    developed database

--- src/main/java/com/fdibaProject/tictactoe/models/GameStatus.java
+++
@@ -1,5 +0,0 @@
-package com.fdibaProject.tictactoe.models;
-
-public enum GameStatus {
-    NEW, IN_PROGRESS, FINISHED;
-}
--- src/main/java/com/fdibaProject/tictactoe/service/GameService.java
+++ src/main/java/com/fdibaProject/tictactoe/service/GameService.java
@@ -1,112 +1,110 @@
 package com.fdibaProject.tictactoe.service;
 
-import com.fdibaProject.tictactoe.exceptions.InvalidGameException;
-import com.fdibaProject.tictactoe.exceptions.InvalidParamException;
-import com.fdibaProject.tictactoe.exceptions.NotFoundException;
-import com.fdibaProject.tictactoe.models.*;
-import com.fdibaProject.tictactoe.storage.GameStorage;
-import lombok.AllArgsConstructor;
+
+import com.fdibaProject.tictactoe.entities.Game;
+import com.fdibaProject.tictactoe.entities.Player;
+import com.fdibaProject.tictactoe.exceptions.InvalidMoveException;
+import com.fdibaProject.tictactoe.exceptions.ResourceNotFoundException;
+import com.fdibaProject.tictactoe.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import static com.fdibaProject.tictactoe.entities.GameStatus.FINISHED;
+import static com.fdibaProject.tictactoe.entities.GameStatus.IN_PROGRESS;
 
 @Service
-@AllArgsConstructor
 public class GameService {
 
-    public Game createGame(Player player){
+    @Autowired
+    private GameRepository gameRepository;
+
+    public Game createGame() {
         Game game = new Game();
-        game.setBoard(new int[3][3]);
-        game.setGameId(UUID.randomUUID().toString());
-        game.setPlayer1(player);
-        game.setStatus(GameStatus.NEW);
-        GameStorage.getInstance().setGame(game);
-        return game;
+        game.setBoard("---------");
+        game.setStatus(IN_PROGRESS);
+        game.setPlayer1(new Player());
+        game.getPlayer1().setSymbol('X');
+        game.setPlayer2(new Player());
+        game.getPlayer2().setSymbol('O');
+        game.setCurrentPlayer(game.getPlayer1());
+        return gameRepository.save(game);
     }
 
-    public Game connectToGame(Player player2, String gameId) throws InvalidParamException, InvalidGameException {
-        if (!GameStorage.getInstance().getGames().containsKey(gameId)){
-            String message = String.format("Game with id %s does not exist!", gameId);
-            throw new InvalidParamException(message);
-        }
-        Game game = GameStorage.getInstance().getGames().get(gameId);
+    public Game updateGame(Long id, int index, Player player) {
+        Game game = gameRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Game not found"));
 
-        if (game.getPlayer2() != null){
-            String message = "Game is not valid anymore.";
-            throw new InvalidGameException(message);
+        if (game.getBoard().charAt(index) != '-') {
+            throw new InvalidMoveException("Invalid move");
         }
 
-        game.setPlayer2(player2);
-        game.setStatus(GameStatus.IN_PROGRESS);
-        GameStorage.getInstance().setGame(game);
-        return game;
-    }
+        char[] boardArray = game.getBoard().toCharArray();
+        boardArray[index] = player.getSymbol();
+        game.setBoard(String.valueOf(boardArray));
 
-    public Game connectToRandomGame (Player player2) throws NotFoundException {
-        Game game = GameStorage.getInstance().getGames().values().stream()
-                .filter(g->g.getStatus().equals(GameStatus.NEW))
-                .findFirst().orElseThrow(()-> new NotFoundException("Game cannot be found!"));
-
-        game.setPlayer2(player2);
-        game.setStatus(GameStatus.IN_PROGRESS);
-        GameStorage.getInstance().setGame(game);
-        return game;
-    }
-
-    public Game gamePlay(GamePlay gamePlay) throws NotFoundException, InvalidGameException {
-        if (!GameStorage.getInstance().getGames().containsKey(gamePlay.getGameId())){
-            String message = String.format("Game with id %s cannot be found!", gamePlay.getGameId());
-            throw new NotFoundException(message);
-        }
-
-        Game game = GameStorage.getInstance().getGames().get(gamePlay.getGameId());
-        if (game.getStatus().equals(GameStatus.FINISHED)){
-            throw new InvalidGameException("Game is already finished!");
-        }
-
-        int [][] board = game.getBoard();
-        board[gamePlay.getCoordinateX()][gamePlay.getCoordinateY()] = gamePlay.getType().getValue();
-
-        Boolean hasWonX = checkWinCondition(board, Signs.X);
-        Boolean hasWonO = checkWinCondition(board, Signs.O);
-        if (hasWonX || hasWonO){
-            game.setStatus(GameStatus.FINISHED);
-            if (hasWonX) {
-                game.setWinner(Signs.X);
+        if (checkWinner(boardArray, player)) {
+            game.setStatus(FINISHED);
+            game.setWinner(player);
+        } else if (checkDraw(boardArray)) {
+            game.setStatus(FINISHED);
+        } else {
+            if(game.getCurrentPlayer().getSymbol()=='X'){
+                game.getCurrentPlayer().setSymbol('O');
             }
-            else {
-                game.setWinner(Signs.O);
+            else{
+                game.getCurrentPlayer().setSymbol('O');
             }
         }
 
-        GameStorage.getInstance().setGame(game);
-        return game;
+        return gameRepository.save(game);
     }
 
-    public boolean checkWinCondition(int [][] board, Signs sign) {
-        int[] boardAsArray = new int[9];
-        int counterIndex = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                boardAsArray[counterIndex] = board[i][j];
-                counterIndex++;
-            }
-        }
 
-        int[][] winCombos = {
-                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 4, 8}, {2, 4, 6}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}
+    private boolean checkWinner(char[] board, Player player) {
+        char symbol = player.getSymbol();
+        int[][] winConditions = {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                {0, 4, 8}, {2, 4, 6}
         };
 
-        for (int i = 0; i < winCombos.length; i++) {
-            int counterOfHits = 0;
-            for (int j = 0; j < winCombos[i].length; j++) {
-                if (boardAsArray[j] == sign.getValue()) {
-                    counterOfHits++;
-                }
-                if (counterOfHits == 3) {
-                    return true;
-                }
+        for (int[] condition : winConditions) {
+            if (board[condition[0]] == symbol &&
+                    board[condition[1]] == symbol &&
+                    board[condition[2]] == symbol) {
+                return true;
             }
         }
         return false;
+    }
+
+    private boolean checkDraw(char[] board) {
+        for (char c : board) {
+            if (c == '-') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+
+    }
+
+    public Game getGameById(Long id) {
+        return gameRepository.findById(id).orElse(null);
+    }
+
+    public Game saveGame(Game game) {
+        return gameRepository.save(game);
+    }
+
+    public void deleteGame(Long id) {
+        gameRepository.deleteById(id);
     }
 }
