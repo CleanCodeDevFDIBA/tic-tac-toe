e300edb7c1e981363c75bee95b16bf33f08d7ab5
Stela Nenkova <stelanenkova18@gmail.com>
2024-04-26 14:08:38 +0200

    Implemented Page with Players Names Inputs

---
+++ src/main/frontend/src/components/pages/Players/Player.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Player.css'
+
+    function Player() {
+        return (
+            <div className='container'>
+                <h1>Enter Players names!</h1>
+                <div className="player-names">
+
+                    <label className="player1">
+                        Player 1 (X):
+                        <input
+                            type="text"
+                            //               onChange={(e) => setPlayer1Name(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        Player 2 (O):
+                        <input
+                            type="text"
+                            //               onChange={(e) => setPlayer2Name(e.target.value)}
+                        />
+                    </label>
+
+
+                </div>
+
+                <div className="buttons">
+                    <div className="startGameButton">
+                        <a href="/GameOnePC">Start the game</a>
+                    </div>
+
+                    <div className="homeButton">
+                        <a href="/">Home</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+        )
+
+    }
+
+export default Player;
