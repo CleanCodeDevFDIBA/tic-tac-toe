81aafb33362b96bdbad76b6a71f730e56ec98628
Mihaela Dobreva <92448497+MihaelaDobreva@users.noreply.github.com>
2024-05-23 18:15:56 +0200

    -tests update

--- src/test/java/com/fdibaProject/tictactoe/TicTacToeApplicationTests.java
+++ src/test/java/com/fdibaProject/tictactoe/TicTacToeApplicationTests.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+/*@SpringBootTest
 @ActiveProfiles("test")
+*/
 class TicTacToeApplicationTests {
 
     /*
